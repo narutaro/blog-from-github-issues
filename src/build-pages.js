feefdd2d196e9050ba3d32f44ed1f4626201d0fc
narutaro <narusan@mac.com>
2022-09-01 16:44:32 +0900

    Update build-pages.js

--- src/build-pages.js
+++ src/build-pages.js
@@ -14,41 +14,42 @@ export target_issue_id=1198469728
 
 // Build
 octokit.rest.issues.listForRepo({
-	owner: process.env.owner,
-	repo: process.env.repo,
+  owner: process.env.owner,
+  repo: process.env.repo,
 })
 .then(issues => {
 
-	issues.owner = process.env.owner
-	issues.repo = process.env.repo
+  issues.owner = process.env.owner
+  issues.repo = process.env.repo
 
-	// Format issue object
-	issues.data.map(issue => issue.updated_at_short = df.shortDate(issue.updated_at))
-	console.log("issues: ", issues)
+  // Format issue object
+  issues.data.map(issue => issue.updated_at_short = df.shortDate(issue.updated_at))
+  console.log("issues: ", issues)
 
-	// Build index
-	const index_template = fs.readFileSync("template/index.template.html", "utf8").toString();
-	const index_html = Mustache.render(index_template, issues)
-	fs.writeFileSync("index.html", index_html, "utf8");
+  // Build index
+  const index_template = fs.readFileSync("template/index.template.html", "utf8").toString();
+  const index_html = Mustache.render(index_template, issues)
+  fs.writeFileSync("index.html", index_html, "utf8");
 
-	// Build post
-	target_issue = issues.data.filter((ti) => {
-		return ti.id == process.env.target_issue_id
-	});
-	target_issue = target_issue[0]
+  // Build post
+  target_issue = issues.data.filter((ti) => {
+    return ti.id == process.env.target_issue_id
+  });
+  target_issue = target_issue[0]
 
-	markdown = target_issue.body
-	const issue_template = fs.readFileSync("template/post.template.html", "utf8").toString();
-  octokit.rest.markdown.render({"text": markdown, "mode": "gfm"})
-	.then(issue_html => {
+  markdown = target_issue.body
+  const issue_template = fs.readFileSync("template/post.template.html", "utf8").toString();
+  // octokit.rest.markdown.render({"text": markdown, "mode": "gfm"})
+  octokit.rest.markdown.render({"text": markdown})
+    .then(issue_html => {
 
-		target_issue.issue_html = issue_html
-		console.log("target_issue: ", target_issue)
+      target_issue.issue_html = issue_html
+      console.log("target_issue: ", target_issue)
 
-		const issue_page = Mustache.render(issue_template, target_issue)
-		console.log('-----------------------------')
-		console.log(issue_page)
-	  fs.writeFileSync("posts/" + process.env.target_issue_id + ".html", issue_page, "utf8");
-	})
+      const issue_page = Mustache.render(issue_template, target_issue)
+      console.log('-----------------------------')
+      console.log(issue_page)
+      fs.writeFileSync("posts/" + process.env.target_issue_id + ".html", issue_page, "utf8");
+  })
 
 });
